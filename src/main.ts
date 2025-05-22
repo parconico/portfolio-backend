5c9bbe55608a8df2e7858a750bd93804671fb5e2
Nicolas <nicolas.parco@taligent.com.ar>
2025-05-22 14:00:40 -0300

    feat: :sparkles: modify main

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
   app.enableCors({
-    origin: 'https://portfolio-nicolas-parco.up.railway.app/',
+    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    credentials: true,
   });
   app.useGlobalPipes(
     new ValidationPipe({
